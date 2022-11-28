9d573e67a7b7a55fcfa25bf125e599a7c397ed3e
dadiazr07 <dadiazr.07@gmail.com>
2022-11-28 18:48:15 +0000

    MyCommit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
 };
+
+function generateExcuse() {
+  let who = ["the dog", "my grandma", "a turtle", "my bird", "my coworker"];
+  let what = ["eat", "pissed", "crushed", "broked", "destroyed"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] + " " + what[whatIndex] + " my homework " + when[whenIndex]
+  );
+}
